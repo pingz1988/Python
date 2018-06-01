e52e4c081857044b05b351e0468336e3123c304f
hjzp <807961446@qq.com>
2018-06-01 14:28:15 +0800

    Create __init__.py

---
+++ src/importDemo/core/consume/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding=utf-8 -*-
+# !/usr/bin/env python
+
+"""
+----------------------------------------------
+ File Name      :       __init__.py
+ Description    :
+ Author         :       pingz
+ Date           :       2018/4/28
+----------------------------------------------
+"""
+
+__author__ = 'pingz'
