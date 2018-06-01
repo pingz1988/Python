645656ff6654c6cb69170f7dca6d5f4760014e8c
hjzp <807961446@qq.com>
2018-06-01 14:29:47 +0800

    Create consume2.py

---
+++ src/importDemo/core/consume/consume2.py
@@ -0,0 +1,17 @@
+# -*- coding=utf-8 -*-
+# !/usr/bin/env python
+
+"""
+----------------------------------------------
+ File Name      :       consume2
+ Description    :
+ Author         :       pingz
+ Date           :       2018/4/28
+----------------------------------------------
+"""
+
+
+__author__ = 'pingz'
+
+if __name__ == "__main__":
+    print("consume 2.")
