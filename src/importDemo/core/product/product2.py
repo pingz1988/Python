a44dec3c507cdffcebbd6e3c75e505b1f2d913b0
hjzp <807961446@qq.com>
2018-06-01 14:32:15 +0800

    Create product2.py

---
+++ src/importDemo/core/product/product2.py
@@ -0,0 +1,16 @@
+# -*- coding=utf-8 -*-
+# !/usr/bin/env python
+
+"""
+----------------------------------------------
+ File Name      :       product2
+ Description    :
+ Author         :       pingz
+ Date           :       2018/4/28
+----------------------------------------------
+"""
+
+__author__ = 'pingz'
+
+if __name__ == "__main__":
+    print("product 2.")
